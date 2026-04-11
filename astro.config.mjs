fa084151f641625ba3dd0b6d577f704cb636f31b
taylan <taylankuscu667@gmail.com>
2026-04-11 23:22:10 +0300

    enable tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://taylankuscu6674-art.github.io',
   base: '/limonchelooa',
+  integrations: [tailwind()],
 });
